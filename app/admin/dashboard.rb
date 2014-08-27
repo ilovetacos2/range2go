234facafb65842109b5a557fd97dee4e1d0accb3
enkhalifapro <enkhalifapro@gmail.com>
2014-08-27 15:33:33 -0700

    update production database

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -30,4 +30,7 @@ ActiveAdmin.register_page "Dashboard" do
     #   end
     # end
   end # content
+
+
+
 end
--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,7 @@ module Prelaunchr
     # Enable the asset pipeline
     config.assets.enabled = true
     config.assets.initialize_on_precompile = false
+    config.assets.precompile += ['active_admin.css','active_admin/print.css', 'active_admin.js']
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
